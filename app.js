bfc88e0dfce5cb29b40c6007e5234050f3cf1f03
Dmitriy Zuev <phoenixmarketing@yandex.ru>
2024-10-14 12:19:15 +0700

    backуy

--- app.js
+++ app.js
@@ -6,15 +6,29 @@ const incomeCategoriesRoutes = require('./routes/category-income.routes');
 const operationsRoutes = require('./routes/operation.routes');
 const balanceRoutes = require('./routes/balance.routes');
 
+
 const app = express();
 
-app.use(express.json());
-app.use(cors());
+// Настройка CORS
+const corsOptions = {
+    origin: 'https://phoenix-intensive.github.io', // Укажите ваш источник
+    methods: ['POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type'],
+};
 
+// Используйте middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// Подключите ваши маршруты
 app.use("/api", authRoutes);
 app.use("/api/categories/expense", expenseCategoriesRoutes);
 app.use("/api/categories/income", incomeCategoriesRoutes);
 app.use("/api/operations", operationsRoutes);
 app.use("/api/balance", balanceRoutes);
 
-app.listen('3000', () => console.log(`Server started`));
+// Обработчик для OPTIONS запросов
+app.options('*', cors(corsOptions));
+
+// Запуск сервера
+app.listen(3000, () => console.log('Server started on port 3000'));
